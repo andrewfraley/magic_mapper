5770f6865f7d013382bb60de3010059bdacbd5e4
Andy Fraley <andrew.fraley@gmail.com>
2022-12-29 13:50:07 -0500

    reorg script to separate the user called functions from the quasi private ones

--- magic_mapper.py
+++ magic_mapper.py
@@ -36,93 +36,6 @@ BUTTONS = {
 INPUT_DEVICE = '/dev/input/event3'
 
 
-def main():
-    """MAIN"""
-
-    button_map = get_button_map()
-    input_loop(button_map=button_map)
-
-
-def input_loop(button_map):
-    # Read from the input device
-    # https://stackoverflow.com/a/16682549/866057
-    infile_path = INPUT_DEVICE
-    input_format = "llHHI"
-    event_size = struct.calcsize(input_format)
-    in_file = open(infile_path, "rb")
-    event = in_file.read(event_size)
-
-    code_wait = None  # Are we waiting for button up
-    code_wait_start = None
-    while event:
-        (tv_sec, tv_usec, type, code, value) = struct.unpack(input_format, event)
-        if code in BUTTONS:
-
-            # print("button code: %s" % code)
-            # print("button value: %s" % value)
-            # print("code_wait: %s" % code_wait)
-            # print("code_wait_start: %s" % code_wait_start)
-
-            if not code_wait and value == 1:  # Start waiting for button up
-                print("%s button down" % BUTTONS[code])
-                code_wait = code
-                code_wait_start = time.time()
-            elif code_wait == code and value == 0:
-
-                # We need to ignore long presses
-                now = time.time()
-                button_hold_duration = now - code_wait_start
-                # print("button_hold_duration: %s" % button_hold_duration)
-
-                code_wait = None
-                code_wait_start = None
-
-                if button_hold_duration < 1.0:
-                    print("%s button up" % BUTTONS[code])
-                    fire_event(code, button_map)
-                else:
-                    print("Ignoring long press of %s" % BUTTONS[code])
-        elif code not in [0, 1] and value == 0:
-            print("Unknown button pressed. (code=%s)" % code)
-        event = in_file.read(event_size)
-
-    in_file.close()
-
-
-def get_button_map():
-    """Read the json config file"""
-    config_path = os.path.join(os.path.dirname(__file__), "magic_mapper_config.json")
-    with open(config_path) as config_file:
-        button_map = json.load(config_file)
-    return button_map
-
-
-def fire_event(code, button_map):
-    print("firing event for code: %s" % code)
-    button_name = BUTTONS[code]
-    print("button_name: %s" % button_name)
-
-    if button_name not in button_map:
-        print("Button %s not configured in magic_mapper_config.json " % button_name)
-        return
-
-    func_name = button_map[button_name]["function"]
-    print("func_name: %s" % func_name)
-    inputs = button_map[button_name].get("inputs", {})
-    globals()[func_name](inputs)
-
-
-def luna_send(endpoint, payload):
-    # Execute luna send and return the output
-
-    command = ["/usr/bin/luna-send", "-n", "1"]
-    command.append(endpoint)
-    command.append(json.dumps(payload))
-    print("running command: %s" % command)
-    output = subprocess.check_output(command)
-    return output
-
-
 def cycle_energy_mode(inputs):
     """cycle energy modes between min med max off"""
     modes = ["max", "med", "min", "off"]
@@ -187,23 +100,6 @@ def reduce_oled_light(inputs):
     increment_oled_light(inputs, direction="down")
 
 
-def increment_oled_light(inputs, direction):
-    increment = inputs.get("increment", 10)
-    current_value = get_picture_settings()["settings"]["backlight"]
-
-    if direction == "up":
-        new_value = current_value + increment
-        if new_value > 100:
-            new_value = 100
-    elif direction == "down":
-        new_value = current_value - increment
-        if new_value < 0:
-            new_value = 0
-
-    inputs["backlight"] = new_value
-    set_oled_backlight(inputs)
-
-
 def set_oled_backlight(inputs):
     backlight = inputs["backlight"]
     endpoint = "luna://com.webos.settingsservice/setSystemSettings"
@@ -211,22 +107,6 @@ def set_oled_backlight(inputs):
     luna_send(endpoint, payload)
     if inputs.get("notifications"):
         show_message("OLED Backlight: %s" % backlight)
-
-
-def get_picture_settings():
-    # Return the current settings
-    endpoint = "luna://com.webos.settingsservice/getSystemSettings"
-    payload = {"category": "picture"}
-    output = luna_send(endpoint, payload)
-    settings = json.loads(output)
-    return settings
-
-
-def show_message(message):
-    """Shows a "toast" message"""
-    endpoint = "luna://com.webos.notification/createToast"
-    payload = {"message": message}
-    luna_send(endpoint, payload)
 
 
 def send_ir_command(inputs):
@@ -304,6 +184,78 @@ def press_button(inputs):
     send_keystroke(INPUT_DEVICE, keycode)
 
 
+###################################
+# Private Functions
+# The fuctions below here should not be called by magic_mapper_config.json
+####################################
+
+def get_button_map():
+    """Read the json config file"""
+    config_path = os.path.join(os.path.dirname(__file__), "magic_mapper_config.json")
+    with open(config_path) as config_file:
+        button_map = json.load(config_file)
+    return button_map
+
+
+def fire_event(code, button_map):
+    print("firing event for code: %s" % code)
+    button_name = BUTTONS[code]
+    print("button_name: %s" % button_name)
+
+    if button_name not in button_map:
+        print("Button %s not configured in magic_mapper_config.json " % button_name)
+        return
+
+    func_name = button_map[button_name]["function"]
+    print("func_name: %s" % func_name)
+    inputs = button_map[button_name].get("inputs", {})
+    globals()[func_name](inputs)
+
+
+def luna_send(endpoint, payload):
+    # Execute luna send and return the output
+
+    command = ["/usr/bin/luna-send", "-n", "1"]
+    command.append(endpoint)
+    command.append(json.dumps(payload))
+    print("running command: %s" % command)
+    output = subprocess.check_output(command)
+    return output
+
+
+def increment_oled_light(inputs, direction):
+    increment = inputs.get("increment", 10)
+    current_value = int(get_picture_settings()["settings"]["backlight"])
+
+    if direction == "up":
+        new_value = current_value + increment
+        if new_value > 100:
+            new_value = 100
+    elif direction == "down":
+        new_value = current_value - increment
+        if new_value < 0:
+            new_value = 0
+
+    inputs["backlight"] = new_value
+    set_oled_backlight(inputs)
+
+
+def get_picture_settings():
+    # Return the current settings
+    endpoint = "luna://com.webos.settingsservice/getSystemSettings"
+    payload = {"category": "picture"}
+    output = luna_send(endpoint, payload)
+    settings = json.loads(output)
+    return settings
+
+
+def show_message(message):
+    """Shows a "toast" message"""
+    endpoint = "luna://com.webos.notification/createToast"
+    payload = {"message": message}
+    luna_send(endpoint, payload)
+
+
 def get_keycode(button):
     """ Returns the keycode associated with the button name """
     keys = [k for k, v in BUTTONS.items() if v == button]
@@ -339,6 +291,59 @@ def send_input_event(device, keycode, value, event_type):
     event = struct.pack(input_format, *data)
     os.write(out_file, event)
     os.close(out_file)
+
+
+def input_loop(button_map):
+    # Read from the input device
+    # https://stackoverflow.com/a/16682549/866057
+    infile_path = INPUT_DEVICE
+    input_format = "llHHI"
+    event_size = struct.calcsize(input_format)
+    in_file = open(infile_path, "rb")
+    event = in_file.read(event_size)
+
+    code_wait = None  # Are we waiting for button up
+    code_wait_start = None
+    while event:
+        (tv_sec, tv_usec, type, code, value) = struct.unpack(input_format, event)
+        if code in BUTTONS:
+
+            # print("button code: %s" % code)
+            # print("button value: %s" % value)
+            # print("code_wait: %s" % code_wait)
+            # print("code_wait_start: %s" % code_wait_start)
+
+            if not code_wait and value == 1:  # Start waiting for button up
+                print("%s button down" % BUTTONS[code])
+                code_wait = code
+                code_wait_start = time.time()
+            elif code_wait == code and value == 0:
+
+                # We need to ignore long presses
+                now = time.time()
+                button_hold_duration = now - code_wait_start
+                # print("button_hold_duration: %s" % button_hold_duration)
+
+                code_wait = None
+                code_wait_start = None
+
+                if button_hold_duration < 1.0:
+                    print("%s button up" % BUTTONS[code])
+                    fire_event(code, button_map)
+                else:
+                    print("Ignoring long press of %s" % BUTTONS[code])
+        elif code not in [0, 1] and value == 0:
+            print("Unknown button pressed. (code=%s)" % code)
+        event = in_file.read(event_size)
+
+    in_file.close()
+
+
+def main():
+    """MAIN"""
+
+    button_map = get_button_map()
+    input_loop(button_map=button_map)
 
 
 if __name__ == "__main__":
